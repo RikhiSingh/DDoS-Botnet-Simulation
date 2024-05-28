d29f3d2e1af8e643156b62c35e969125365cadcb
rikhisingh <rikhisinghnov@gmail.com>
2024-05-27 23:43:44 -0400

    Added comments

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+# Selenium Imports
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+# Webdriver Imports
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Time Import
 import time
 
-# Set up the webdriver
+# Set up the webdriver (using chrome for thi)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 try:
